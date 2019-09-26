db49d194eccc64978c4b43c292342baa91c345a9
Ryan Odegard <rsodegard@gmail.com>
2019-09-26 18:24:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,21 +40,32 @@ def map_to_double(source_array)
   p new_array
 end 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   i = 0 
-  total = 0 
   while i < source_array.length do 
-    total += source_array[i]
+    starting_point += source_array[i]
     i += 1 
   end 
-  p total 
+  p starting_point 
 end 
 
 def reduce_to_all_true(source_array)
-
+  i = 0 
+  while i < source_array.length do 
+    return false if !source_array[i]
+    i += 1
+  end 
+  return true 
 end 
 
-def reduce_any_to_true(source_array)
-
+def reduce_to_any_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    return true if source_array[i]
+    i += 1 
+  end 
+  return false 
 end 
+
+
 
